e052254a72a6555f9db44edcd6a23e8feb8e044d
Bin-Choi <csb5268@gmail.com>
2025-02-15 23:23:47 +0900

    feat: 프로필 수정 api

--- src/main/java/com/mockApi/api/controller/ProfileController.java
+++ src/main/java/com/mockApi/api/controller/ProfileController.java
@@ -1,12 +1,13 @@
 package com.mockApi.api.controller;
 
+import com.mockApi.api.dto.ProfileRequestDto;
 import com.mockApi.api.dto.ProfileResponseDto;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import org.apache.coyote.Response;
 
 import java.util.List;
 
@@ -35,4 +36,30 @@ public class ProfileController {
 
         return ResponseEntity.ok(response);
     }
+
+    @PutMapping("/my")
+    public ResponseEntity<String> updateProfile(@RequestBody ProfileRequestDto request) {
+        String msg = validateProfile(request);
+        if (msg != null) {
+            return ResponseEntity.badRequest().body(msg);
+        }
+
+        return ResponseEntity.ok("프로필 변경 성공");
+    }
+
+    private String validateProfile(ProfileRequestDto requestDto) {
+
+        if (requestDto.getIntroduce() != null && requestDto.getIntroduce().length() > 100) {
+            return "소개 글자 수는 최대 100자까지 가능합니다.";
+        }
+        if (requestDto.getHair() == 0 || requestDto.getEye() == 0 ||
+                requestDto.getMouse() == 0 || requestDto.getSkin() == 0 ||
+                requestDto.getNickColor() == 0) {
+            return "아이템을 장착해야 합니다.";
+        }
+
+        return null; // 검증 통과
+    }
+
+
 }
--- src/main/java/com/mockApi/api/dto/ProfileRequestDto.java
+++ src/main/java/com/mockApi/api/dto/ProfileRequestDto.java
@@ -1,5 +1,18 @@
 package com.mockApi.api.dto;
 
-public class ProfileRequestDto {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+public class ProfileRequestDto {
+    private String username;
+    private String introduce;
+    private int hair;
+    private int eye;
+    private int mouse;
+    private int skin;
+    private int nickColor;
 }
