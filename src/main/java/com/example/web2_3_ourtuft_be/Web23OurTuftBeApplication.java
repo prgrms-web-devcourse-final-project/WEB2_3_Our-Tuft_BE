fed94c5867de04b601a6ee487bad0b17a9177985
DoHyun <citycozyboy@gmail.com>
2025-02-17 16:30:53 +0900

    ✨Feature #81 : 초기 프로젝트 설정 및 필수 라이브러리 설치

---
+++ src/main/java/com/example/web2_3_ourtuft_be/Web23OurTuftBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.web2_3_ourtuft_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web23OurTuftBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Web23OurTuftBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/web2_3_ourtuft_be/Web23OurTuftBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web2_3_ourtuft_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web23OurTuftBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
