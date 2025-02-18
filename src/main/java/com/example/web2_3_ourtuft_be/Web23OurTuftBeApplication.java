b30e501f6d64171c0522c928dcf1bad6334d04e0
Bin-Choi <109321881+Bin-Choi@users.noreply.github.com>
2025-02-18 09:21:05 +0900

    DB 연결 (#7)

--- src/main/java/com/example/web2_3_ourtuft_be/Web23OurTuftBeApplication.java
+++ src/main/java/com/example/web2_3_ourtuft_be/Web23OurTuftBeApplication.java
@@ -2,8 +2,10 @@ package com.example.web2_3_ourtuft_be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:.env")
 public class Web23OurTuftBeApplication {
 
     public static void main(String[] args) {
