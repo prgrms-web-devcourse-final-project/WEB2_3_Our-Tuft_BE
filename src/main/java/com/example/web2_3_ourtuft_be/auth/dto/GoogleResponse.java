9986f29d956d9537b062ef4d35f5987942d66ca5
Bin-Choi <csb5268@gmail.com>
2025-02-19 12:04:58 +0900

    style: 들여쓰기 수정 - 2자 -> 4자

--- src/main/java/com/example/web2_3_ourtuft_be/auth/dto/GoogleResponse.java
+++ src/main/java/com/example/web2_3_ourtuft_be/auth/dto/GoogleResponse.java
@@ -5,29 +5,29 @@ import java.util.Map;
 
 public class GoogleResponse implements OAuth2Response {
 
-  private final Map<String, Object> attributes;
+    private final Map<String, Object> attributes;
 
-  public GoogleResponse(Map<String, Object> attributes) {
-    this.attributes = attributes;
-  }
+    public GoogleResponse(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
 
-  @Override
-  public Provider getProvider() {
-    return Provider.GOOGLE;
-  }
+    @Override
+    public Provider getProvider() {
+        return Provider.GOOGLE;
+    }
 
-  @Override
-  public String getProviderId() {
-    return attributes.get("sub").toString();
-  }
+    @Override
+    public String getProviderId() {
+        return attributes.get("sub").toString();
+    }
 
-  @Override
-  public String getEmail() {
-    return attributes.get("email").toString();
-  }
+    @Override
+    public String getEmail() {
+        return attributes.get("email").toString();
+    }
 
-  @Override
-  public String getName() {
-    return attributes.get("name").toString();
-  }
+    @Override
+    public String getName() {
+        return attributes.get("name").toString();
+    }
 }
