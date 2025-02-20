85b367acac5de96c4f4842c6b348d9cfac1f2067
Bin-Choi <csb5268@gmail.com>
2025-02-20 12:56:04 +0900

    test: 닉네임 검증 테스트 - 닉네임 null, 공백 테스트 - 영어/한글/숫자 조합 테스트 - 길이 테스트

---
+++ src/test/java/com/example/web2_3_ourtuft_be/user/entity/NicknameTest.java
@@ -0,0 +1,82 @@
+package com.example.web2_3_ourtuft_be.user.entity;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.example.web2_3_ourtuft_be.global.exception.exceptions.InvalidValueException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class NicknameTest {
+
+    @Test
+    @DisplayName("닉네임 null, 공백 예외 발생 테스트")
+    void invalidNicknameTest1() {
+        // Given
+        String blank = "";
+        String n = null;
+
+        // When & Then
+        assertThrows(
+                InvalidValueException.class,
+                () -> {
+                    new Nickname(blank);
+                    new Nickname(n);
+                });
+    }
+
+    @Test
+    @DisplayName("영/한/숫자 이외의 문자 예외 발생 테스트")
+    void invalidNicknameTest2() {
+        // Given
+        String input = "!#@ad";
+
+        // When & Then
+        assertThrows(
+                InvalidValueException.class,
+                () -> {
+                    new Nickname(input);
+                });
+    }
+
+    @Test
+    @DisplayName("길이 예외 테스트(1글자)")
+    void invalidNicknameTest3() {
+        // Given
+        String input = "a";
+
+        // When & Then
+        assertThrows(
+                InvalidValueException.class,
+                () -> {
+                    new Nickname(input);
+                });
+    }
+
+    @Test
+    @DisplayName("길이 예외 테스트(11글자)")
+    void invalidNicknameTest4() {
+        // Given
+        String input = "abcdefghqwe";
+
+        // When & Then
+        assertThrows(
+                InvalidValueException.class,
+                () -> {
+                    new Nickname(input);
+                });
+    }
+
+    @Test
+    @DisplayName("정상 생성")
+    void validNameTest() {
+        // Given
+        String input = "tester1";
+
+        // When & Then
+        assertDoesNotThrow(
+                () -> {
+                    new Nickname(input);
+                });
+    }
+}
