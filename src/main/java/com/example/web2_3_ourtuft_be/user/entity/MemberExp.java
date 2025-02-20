53fd8b017f640e920a78e7cb3d29e24a77dc571e
Bin-Choi <csb5268@gmail.com>
2025-02-21 03:03:06 +0900

    feat: 유저 경험치 도메인 생성#129 - 경험치 증가 - 경험치 관련 필드 조회

---
+++ src/main/java/com/example/web2_3_ourtuft_be/user/entity/MemberExp.java
@@ -0,0 +1,36 @@
+package com.example.web2_3_ourtuft_be.user.entity;
+
+import com.example.web2_3_ourtuft_be.common.BaseTime;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Entity
+@Table(name = "member_levels")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+public class MemberExp extends BaseTime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "level_id")
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    private int exp;
+
+    public void increaseExp(int exp) {
+        this.exp += exp;
+    }
+
+    public int getLevel() {
+        return exp / 100;
+    }
+
+    public int getProgress() {
+        return exp % 100;
+    }
+    // 일단 각 레벨별 필요 경험치는 100으로 설정
+}
