e2a8549cfd82b797cf45bf2695e933faa7a0f793
ChaeeunnPark <roseehdtod@naver.com>
2025-02-20 14:52:29 +0900

    feat: 아이템 조회 # 108

--- src/main/java/com/example/web2_3_ourtuft_be/item/entity/enums/ItemAppearanceType.java
+++
@@ -1,6 +0,0 @@
-package com.example.web2_3_ourtuft_be.item.entity.enums;
-
-public enum ItemAppearanceType {
-    IMAGE,
-    COLOR
-}
--- src/main/java/com/example/web2_3_ourtuft_be/item/repository/ItemRepository.java
+++ src/main/java/com/example/web2_3_ourtuft_be/item/repository/ItemRepository.java
@@ -1,8 +1,17 @@
 package com.example.web2_3_ourtuft_be.item.repository;
 
 import com.example.web2_3_ourtuft_be.item.entity.Item;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ItemRepository extends JpaRepository<Item, Long> {}
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    Slice<Item> findByCategoryAndNameContaining(String category, String keyword, Pageable pageable);
+
+    Slice<Item> findByCategory(String category, Pageable pageable);
+
+    Slice<Item> findByNameContaining(String keyword, Pageable pageable);
+}
