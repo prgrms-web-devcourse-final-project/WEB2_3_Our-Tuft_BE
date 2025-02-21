3aa2c03a08d01c28d9f3bb3b667d01e57dc49547
youngsuh <yseokim0212@gmail.com>
2025-02-21 11:35:39 +0900

    refactor:방검색 API수정

--- src/main/java/com/example/web2_3_ourtuft_be/lobby/controller/lobbyController.java
+++ src/main/java/com/example/web2_3_ourtuft_be/lobby/controller/lobbyController.java
@@ -1,16 +1,11 @@
 package com.example.web2_3_ourtuft_be.lobby.controller;
 
-import com.example.web2_3_ourtuft_be.global.exception.messages.InvalidRequestMessages;
-import com.example.web2_3_ourtuft_be.global.exception.messages.NotFoundMessages;
+import com.example.web2_3_ourtuft_be.global.response.GlobalResponse;
 import com.example.web2_3_ourtuft_be.lobby.service.LobbyService;
 import com.example.web2_3_ourtuft_be.room.dto.RoomResponseDto;
-import com.example.web2_3_ourtuft_be.room.entity.Room;
-import com.example.web2_3_ourtuft_be.room.repository.RoomRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,49 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class lobbyController {
 
     private final LobbyService lobbyService;
-    private final RoomRepository roomRepository;
 
     @Operation(summary = "방 검색", description = "방 제목과 ID로 검색하는 API입니다.")
     @GetMapping("/room")
-    public ResponseEntity<?> searchRoom(
+    public ResponseEntity<GlobalResponse<List<RoomResponseDto>>> searchRoom(
             @RequestParam(required = false) String roomName,
             @RequestParam(required = false) Long roomId) {
 
-        if (roomId != null) {
-            return searchRoomById(roomId);
-        } else if (roomName != null) {
-            return searchRoomByName(roomName);
-        }
-
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(InvalidRequestMessages.EMPTY_SEARCH_CONDITION.getMessage());
-    }
-
-    public ResponseEntity<?> searchRoomById(Long roomId) {
-
-        Room room = roomRepository.findById(roomId).orElse(null);
-
-        if (room == null) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body(NotFoundMessages.ROOM_ID.getMessage());
-        }
-
-        RoomResponseDto response = new RoomResponseDto(room);
-        return ResponseEntity.ok(response);
-    }
-
-    public ResponseEntity<?> searchRoomByName(String roomName) {
-
-        List<Room> rooms = roomRepository.findRoomNameContaining(roomName);
-
-        if (rooms.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body(NotFoundMessages.ROOM_NAME.getMessage());
-        }
-
-        List<RoomResponseDto> responseList =
-                rooms.stream().map(RoomResponseDto::new).collect(Collectors.toList());
-
-        return ResponseEntity.ok(responseList);
+        return ResponseEntity.ok(GlobalResponse.success(lobbyService.searchRoom(roomName, roomId)));
     }
 }
--- src/main/java/com/example/web2_3_ourtuft_be/lobby/service/LobbyService.java
+++ src/main/java/com/example/web2_3_ourtuft_be/lobby/service/LobbyService.java
@@ -1,11 +1,16 @@
 package com.example.web2_3_ourtuft_be.lobby.service;
 
+import com.example.web2_3_ourtuft_be.global.exception.exceptions.GlobalException;
+import com.example.web2_3_ourtuft_be.global.exception.messages.InvalidRequestMessages;
+import com.example.web2_3_ourtuft_be.global.exception.messages.NotFoundMessages;
 import com.example.web2_3_ourtuft_be.room.dto.RoomResponseDto;
 import com.example.web2_3_ourtuft_be.room.entity.Room;
 import com.example.web2_3_ourtuft_be.room.repository.RoomRepository;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,24 +19,34 @@ public class LobbyService {
 
     private final RoomRepository roomRepository;
 
-    public List<RoomResponseDto> searchRoomByName(String roomName) {
-        List<Room> rooms;
+    public List<RoomResponseDto> searchRoom(String roomName, Long roomId) {
 
-        if (roomName != null && !roomName.isEmpty()) {
+        List<Room> rooms = new ArrayList<>();
+
+        if (roomId != null) {
+
+            Room room = roomRepository.findById(roomId).orElse(null);
+            if (room == null) {
+                throw new GlobalException(
+                        NotFoundMessages.ROOM_ID.getMessage(), HttpStatus.NOT_FOUND);
+            }
+            rooms.add(room);
+
+        } else if (roomName != null) {
+
             rooms = roomRepository.findRoomNameContaining(roomName);
+
+            if (rooms.isEmpty()) {
+                throw new GlobalException(
+                        NotFoundMessages.ROOM_NAME.getMessage(), HttpStatus.NOT_FOUND);
+            }
+
         } else {
-            rooms = roomRepository.findAll();
+            throw new GlobalException(
+                    InvalidRequestMessages.EMPTY_SEARCH_CONDITION.getMessage(),
+                    HttpStatus.BAD_REQUEST);
         }
 
-        return rooms.stream()
-                .map(
-                        room ->
-                                new RoomResponseDto(
-                                        room.getId(),
-                                        room.getRoomName(),
-                                        room.getPeopleEntering(),
-                                        room.getRound(),
-                                        room.getGameStatus()))
-                .collect(Collectors.toList());
+        return rooms.stream().map(RoomResponseDto::new).collect(Collectors.toList());
     }
 }
