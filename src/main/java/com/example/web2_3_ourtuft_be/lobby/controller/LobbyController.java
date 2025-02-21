77f70fdbf929faaabf1670a85689a1780cb4736a
youngsuh <yseokim0212@gmail.com>
2025-02-21 17:17:01 +0900

    rename:클래스 이름 변경

--- src/main/java/com/example/web2_3_ourtuft_be/lobby/controller/LobbyController.java
+++ src/main/java/com/example/web2_3_ourtuft_be/lobby/controller/LobbyController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class lobbyController {
+public class LobbyController {
 
     private final LobbyService lobbyService;
 
