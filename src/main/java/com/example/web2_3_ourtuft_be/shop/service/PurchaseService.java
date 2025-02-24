2534f75e82dfcfecd9851a14423607db9356f3be
ChaeeunnPark <roseehdtod@naver.com>
2025-02-24 11:36:45 +0900

    refactor: 구매, 포인트 service 분리

--- src/main/java/com/example/web2_3_ourtuft_be/shop/service/PurchaseService.java
+++ src/main/java/com/example/web2_3_ourtuft_be/shop/service/PurchaseService.java
@@ -1,19 +1,9 @@
 package com.example.web2_3_ourtuft_be.shop.service;
 
-import com.example.web2_3_ourtuft_be.item.entity.Item;
 import com.example.web2_3_ourtuft_be.item.service.ItemService;
-import com.example.web2_3_ourtuft_be.shop.entity.PurchaseHistory;
-import com.example.web2_3_ourtuft_be.shop.entity.PurchaseItem;
-import com.example.web2_3_ourtuft_be.shop.repository.PurchaseHistoryRepository;
-import com.example.web2_3_ourtuft_be.shop.repository.PurchaseItemRepository;
-import com.example.web2_3_ourtuft_be.user.entity.MemberPoint;
-import com.example.web2_3_ourtuft_be.user.entity.PointHistory;
 import com.example.web2_3_ourtuft_be.user.entity.enums.PointChangeReason;
-import com.example.web2_3_ourtuft_be.user.repository.MemberPointRepository;
-import com.example.web2_3_ourtuft_be.user.repository.PointHistoryRepository;
 import com.example.web2_3_ourtuft_be.user.service.MemberPointService;
 import jakarta.transaction.Transactional;
-import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -22,9 +12,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class PurchaseService {
 
-    private final PurchaseHistoryRepository purchaseHistoryRepository;
-    private final PurchaseItemRepository purchaseItemRepository;
-    private final PointHistoryRepository pointHistoryRepository;
+    private final PurchaseHistoryService purchaseHistoryService;
     private final MemberPointService memberPointService;
     private final ItemService itemService;
 
@@ -36,53 +24,12 @@ public class PurchaseService {
         int totalPrice = calculateTotalPrice(items);
         int totalQuantity = items.size();
 
-        processPointUsage(userId, totalPrice);
+        memberPointService.updatePoints(userId, -totalPrice, PointChangeReason.PURCHASE);
 
-        PurchaseHistory purchaseHistory = savePurchaseHistory(userId, totalPrice, totalQuantity);
-
-        savePurchaseItems(purchaseHistory.getId(), items);
+        purchaseHistoryService.savePurchaseHistory(userId, items, totalPrice, totalQuantity);
     }
 
     private int calculateTotalPrice(List<Long> items) {
         return items.stream().mapToInt(itemId -> itemService.getItem(itemId).getPrice()).sum();
-    }
-
-    private void processPointUsage(Long userId, int totalPrice) {
-
-        MemberPoint memberPoint = memberPointService.getPoint(userId);
-
-        memberPointService.updatePoints(userId, -totalPrice);
-
-        PointHistory pointHistory =
-                PointHistory.builder()
-                        .memberPointId(memberPoint.getId())
-                        .pointChange(-totalPrice)
-                        .reason(PointChangeReason.PURCHASE.name())
-                        .usageTime(LocalDateTime.now())
-                        .build();
-        pointHistoryRepository.save(pointHistory);
-    }
-
-    private PurchaseHistory savePurchaseHistory(Long userId, int totalPrice, int totalQuantity) {
-        PurchaseHistory purchaseHistory =
-                PurchaseHistory.builder()
-                        .userId(userId)
-                        .totalPrice(totalPrice)
-                        .totalQuantity(totalQuantity)
-                        .purchasedAt(LocalDateTime.now())
-                        .build();
-
-        return purchaseHistoryRepository.save(purchaseHistory);
-    }
-
-    private void savePurchaseItems(Long purchaseHistoryId, List<Long> items) {
-        for (Long itemId : items) {
-            PurchaseItem purchaseItem =
-                    PurchaseItem.builder()
-                            .purchaseHistoryId(purchaseHistoryId)
-                            .itemId(itemId)
-                            .build();
-            purchaseItemRepository.save(purchaseItem);
-        }
     }
 }
--- src/main/java/com/example/web2_3_ourtuft_be/user/service/MemberPointService.java
+++ src/main/java/com/example/web2_3_ourtuft_be/user/service/MemberPointService.java
@@ -1,15 +1,12 @@
 package com.example.web2_3_ourtuft_be.user.service;
 
-import com.example.web2_3_ourtuft_be.global.exception.exceptions.DuplicatedException;
 import com.example.web2_3_ourtuft_be.global.exception.exceptions.NotFoundException;
-import com.example.web2_3_ourtuft_be.global.exception.messages.DuplicatedMessages;
 import com.example.web2_3_ourtuft_be.global.exception.messages.NotFoundMessages;
 import com.example.web2_3_ourtuft_be.user.dto.MyPointsResponseDto;
 import com.example.web2_3_ourtuft_be.user.entity.MemberPoint;
-import com.example.web2_3_ourtuft_be.user.entity.PointHistory;
+import com.example.web2_3_ourtuft_be.user.entity.enums.PointChangeReason;
 import com.example.web2_3_ourtuft_be.user.repository.MemberPointRepository;
-import com.example.web2_3_ourtuft_be.user.repository.PointHistoryRepository;
-import java.util.List;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +15,7 @@ import org.springframework.stereotype.Service;
 public class MemberPointService {
 
     private final MemberPointRepository memberPointRepository;
-    private final PointHistoryRepository pointHistoryRepository;
+    private final PointHistoryService pointHistoryService;
 
     public MemberPoint getPoint(Long userId) {
         return memberPointRepository
@@ -29,40 +26,19 @@ public class MemberPointService {
     public MyPointsResponseDto getMyPoints() {
         Long userId = 1L;
 
-        validateUserPoints();
+        pointHistoryService.validateUserPoints();
 
         return new MyPointsResponseDto(getPoint(userId).getPoints());
     }
 
-    public void updatePoints(Long userId, int amount) {
+    @Transactional
+    public void updatePoints(Long userId, int amount, PointChangeReason reason) {
         MemberPoint memberPoint = getPoint(userId);
 
         int updatedPoints = memberPoint.getPoints() + amount;
 
         memberPoint.updatePoints(updatedPoints);
-        memberPointRepository.save(memberPoint);
-    }
 
-    public void validateUserPoints() {
-        Long userId = 1L;
-
-        MemberPoint memberPoint =
-                memberPointRepository
-                        .findByUserId(userId)
-                        .orElseThrow(() -> new NotFoundException(NotFoundMessages.USER));
-
-        int storedPoints = memberPoint.getPoints();
-
-        List<PointHistory> pointHistoryList =
-                pointHistoryRepository.findByMemberPointId(memberPoint.getId());
-
-        int calculatedPoints = storedPoints;
-        for (PointHistory pointHistory : pointHistoryList) {
-            calculatedPoints += pointHistory.getPointChange();
-        }
-
-        if (calculatedPoints != storedPoints) {
-            throw new DuplicatedException(DuplicatedMessages.MISMATCH_POINT);
-        }
+        pointHistoryService.savePointHistory(memberPoint.getId(), amount, reason);
     }
 }
