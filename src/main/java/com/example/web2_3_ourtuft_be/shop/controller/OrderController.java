2f50602379d395706419232af363324cce51b37e
ChaeeunnPark <roseehdtod@naver.com>
2025-02-28 17:29:58 +0900

    comment: swagger 설명 추가

--- src/main/java/com/example/web2_3_ourtuft_be/shop/controller/OrderController.java
+++ src/main/java/com/example/web2_3_ourtuft_be/shop/controller/OrderController.java
@@ -10,6 +10,8 @@ import com.example.web2_3_ourtuft_be.user.dto.WishItemRequestDto;
 import com.example.web2_3_ourtuft_be.user.entity.User;
 import com.example.web2_3_ourtuft_be.user.service.UserFacadeService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
@@ -28,6 +30,11 @@ public class OrderController {
     private final UserFacadeService userFacadeService;
 
     @Operation(summary = "아이템 구매 API", description = "상점에서 아이템을 구매합니다.")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "성공"),
+        @ApiResponse(responseCode = "404", description = "아이템이 존재하지 않습니다."),
+        @ApiResponse(responseCode = "400", description = "보유 포인트가 부족합니다."),
+    })
     @PostMapping("/order")
     public ResponseEntity<GlobalResponse<OrderListResponse>> order(
             @RequestBody OrderRequest request,
@@ -38,6 +45,10 @@ public class OrderController {
     }
 
     @Operation(summary = "아이템 찜 API", description = "상점에서 아이템을 찜합니다.")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "성공"),
+        @ApiResponse(responseCode = "409", description = "이미 찜한 상품입니다."),
+    })
     @PostMapping("/wishlist")
     public ResponseEntity<GlobalResponse<String>> addWishItem(
             @RequestBody WishItemRequestDto request,
@@ -47,6 +58,10 @@ public class OrderController {
     }
 
     @Operation(summary = "아이템 찜 취소 API", description = "상점 찜 목록에서 아이템을 찜 취소합니다.")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "성공"),
+        @ApiResponse(responseCode = "404", description = "찜한 상품이 존재하지 않습니다.")
+    })
     @DeleteMapping("/wishlist/{itemId}")
     public ResponseEntity<GlobalResponse<String>> deleteWishItem(
             @PathVariable Long itemId, @AuthenticationPrincipal(expression = "user") User user) {
@@ -55,6 +70,7 @@ public class OrderController {
     }
 
     @Operation(summary = "아이템 찜 목록 API", description = "상점 찜 목록을 불러옵니다.")
+    @ApiResponses({@ApiResponse(responseCode = "200", description = "성공")})
     @GetMapping("/wishlist")
     public ResponseEntity<GlobalResponse<PageResponse<ItemResponse>>> wishItemList(
             @RequestParam(value = "page", defaultValue = "0") int page,
