422c953dff71953ab44de6c3b0d707f4d3a110d2
Bin-Choi <csb5268@gmail.com>
2025-03-07 04:18:33 +0900

    feat: Avatar VO 생성 - 장착 아이템의 id 만을 가지고 있는 Avatar 생성

---
+++ src/main/java/com/example/web2_3_ourtuft_be/user/value/Avatar.java
@@ -0,0 +1,38 @@
+package com.example.web2_3_ourtuft_be.user.value;
+
+import jakarta.persistence.Embeddable;
+import lombok.Builder;
+import lombok.Getter;
+
+@Embeddable
+@Getter
+public class Avatar {
+    private final Long eyeItemId;
+    private final Long mouseItemId;
+    private final Long skinItemId;
+    private final Long nicknameItemId;
+
+    public Avatar() {
+        this.eyeItemId = 1L;
+        this.mouseItemId = 4L;
+        this.skinItemId = 7L;
+        this.nicknameItemId = 10L;
+    }
+
+    @Builder
+    private Avatar(Long eyeItemId, Long mouseItemId, Long skinItemId, Long nicknameItemId) {
+        this.eyeItemId = eyeItemId;
+        this.mouseItemId = mouseItemId;
+        this.skinItemId = skinItemId;
+        this.nicknameItemId = nicknameItemId;
+    }
+
+    public Avatar update(Long eyeItemId, Long mouseItemId, Long skinItemId, Long nicknameItemId) {
+        return Avatar.builder()
+                     .eyeItemId(eyeItemId)
+                     .mouseItemId(mouseItemId)
+                     .skinItemId(skinItemId)
+                     .nicknameItemId(nicknameItemId)
+                     .build();
+    }
+}
