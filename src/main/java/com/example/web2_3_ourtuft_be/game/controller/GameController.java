af66ff8c373bbe08b55693f1aa5c28da32f6efe7
DoHyun <citycozyboy@gmail.com>
2025-03-07 19:46:14 +0900

    퀴즈 캐싱 로직 (#87)

--- src/main/java/com/example/web2_3_ourtuft_be/game/controller/GameController.java
+++ src/main/java/com/example/web2_3_ourtuft_be/game/controller/GameController.java
@@ -1,12 +1,8 @@
 package com.example.web2_3_ourtuft_be.game.controller;
 
 import com.example.web2_3_ourtuft_be.game.service.GameService;
-import com.example.web2_3_ourtuft_be.global.response.GlobalResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,11 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class GameController {
 
     private final GameService gameService;
-
-    @PostMapping("/start/{roomId}")
-    public ResponseEntity<GlobalResponse<String>> startGame(@PathVariable Long roomId) {
-        gameService.gameSet(roomId);
-
-        return ResponseEntity.ok(GlobalResponse.success("Game started"));
-    }
+    //
+    //    @PostMapping("/start/{roomId}")
+    //    public ResponseEntity<GlobalResponse<String>> startGame(@PathVariable Long roomId) {
+    //        gameService.gameSet(roomId);
+    //
+    //        return ResponseEntity.ok(GlobalResponse.success("Game started"));
+    //    }
 }
--- src/main/java/com/example/web2_3_ourtuft_be/room/controller/RoomController.java
+++ src/main/java/com/example/web2_3_ourtuft_be/room/controller/RoomController.java
@@ -2,17 +2,17 @@ package com.example.web2_3_ourtuft_be.room.controller;
 
 import com.example.web2_3_ourtuft_be.global.response.GlobalResponse;
 import com.example.web2_3_ourtuft_be.redis.service.ParticipantService;
+import com.example.web2_3_ourtuft_be.redis.service.RoomQuizService;
 import com.example.web2_3_ourtuft_be.room.dto.RoomResponseDto;
 import com.example.web2_3_ourtuft_be.room.service.RoomService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,6 +22,7 @@ public class RoomController {
 
     private final RoomService roomService;
     private final ParticipantService participantService;
+    private final RoomQuizService roomQuizService;
 
     @Operation(summary = "방 참가자 목록 조회", description = "현재 방의 참가자 목록을 조회합니다.")
     @GetMapping("/{roomId}/players")
@@ -29,5 +30,14 @@ public class RoomController {
             @PathVariable("roomId") String roomId) {
         return ResponseEntity.ok(
                 GlobalResponse.success(participantService.getPlayersInRoom(roomId)));
+    }
+
+    @Operation(summary = "게임에서 진행할 퀴즈세트 세팅", description = "퀴즈 목록 중 진행할 퀴즈 세트를 지정합니다. ")
+    @ApiResponses({@ApiResponse(responseCode = "200", description = "성공")})
+    @PutMapping("/{roomId}/quizzes/{quizSetId}")
+    public ResponseEntity<GlobalResponse<String>> setQuizSet(
+            @PathVariable("roomId") Long roomId, @PathVariable("quizSetId") Long quizSetId) {
+        roomQuizService.setQuizSet(roomId, quizSetId);
+        return ResponseEntity.ok(GlobalResponse.success("퀴즈 세트 지정 완료"));
     }
 }
